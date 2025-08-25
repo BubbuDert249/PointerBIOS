c3091f7eb6d8c9cf6947dbaaf4c968e49450e9b1
Bubbu Dert <139119578+BubbuDert249@users.noreply.github.com>
2025-08-25 22:30:38 +0200

    Create PointerBIOS_Pascal.pas

---
+++ PointerBIOS_Pascal.pas
@@ -0,0 +1,95 @@
+unit PointerBIOS;
+
+interface
+uses Dos;
+
+procedure PointerBIOS_Init(bmpPath: string);
+procedure PointerBIOS_Remove;
+
+implementation
+
+var
+  CursorX, CursorY: Word;
+  OldX, OldY: Word;
+  CursorW, CursorH: Word;
+  PointerBIOS_Running: Boolean;
+  CursorBitmap: array of LongWord;
+  FrameBuffer: ^LongWord = Ptr($E0000000,0);
+
+procedure LoadBMP(bmpPath: string);
+var f: File;
+    size, pixOffset, x, y: LongWord;
+    buffer: array of Byte;
+    pix: ^LongWord;
+begin
+  Assign(f, bmpPath);
+  Reset(f,1);
+  size := FileSize(f);
+  SetLength(buffer, size);
+  BlockRead(f, buffer[0], size);
+  Close(f);
+
+  CursorW := PLongWord(@buffer[18])^;
+  CursorH := PLongWord(@buffer[22])^;
+  pixOffset := PLongWord(@buffer[10])^;
+
+  SetLength(CursorBitmap, CursorW*CursorH);
+  pix := @buffer[pixOffset];
+
+  for y := 0 to CursorH-1 do
+    for x := 0 to CursorW-1 do
+      CursorBitmap[y*CursorW+x] := pix[(CursorH-1-y)*CursorW+x]; // flip vertically
+end;
+
+procedure DrawCursor;
+var x,y: Integer; pix: LongWord;
+begin
+  for y := 0 to CursorH-1 do
+    for x := 0 to CursorW-1 do
+    begin
+      pix := CursorBitmap[y*CursorW+x];
+      if (pix shr 24) = 0 then Continue; // alpha=0 = transparent
+      FrameBuffer^[(CursorY+y)*1024 + (CursorX+x)] := pix;
+    end;
+end;
+
+procedure EraseCursor;
+var x,y: Integer;
+begin
+  for y := 0 to CursorH-1 do
+    for x := 0 to CursorW-1 do
+      FrameBuffer^[(OldY+y)*1024 + (OldX+x)] := 0;
+end;
+
+procedure PointerBIOS_Init(bmpPath: string);
+var regs: Registers;
+begin
+  { Set VESA 1024x768x32 }
+  regs.ax := $4F02;
+  regs.bx := $118;
+  Intr($10, regs);
+
+  LoadBMP(bmpPath);
+  PointerBIOS_Running := True;
+
+  while PointerBIOS_Running do
+  begin
+    OldX := CursorX; OldY := CursorY;
+
+    regs.ax := 3;
+    Intr($33, regs);
+    CursorX := regs.cx;
+    CursorY := regs.dx;
+
+    EraseCursor;
+    DrawCursor;
+  end;
+end;
+
+procedure PointerBIOS_Remove;
+begin
+  PointerBIOS_Running := False;
+  EraseCursor;
+end;
+
+end.
